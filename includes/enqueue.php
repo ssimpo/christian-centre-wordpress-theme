f8d149bec972c848b372995bb2568f0ddbb3606a
Stephen Simpson <me@simpo.org>
2013-09-11 19:45:25 +0100

    Added enqueuing scripts and dojo loader to the theme.

---
+++ includes/enqueue.php
@@ -0,0 +1,53 @@
+<?php
+function RPRHAG_script_config(){
+    wp_register_script(
+        'RPRHAGConfig',
+        get_template_directory_uri().'/scripts/jsConfig.php',
+        array()
+    );
+   
+    wp_enqueue_script('RPRHAGConfig');
+}
+
+function RPRHAG_script_loader(){
+    wp_register_script(
+        'dojo',
+        '//ajax.googleapis.com/ajax/libs/dojo/1.9.1/dojo/dojo.js',
+        array('RPRHAGConfig'),
+        '1.9.1'
+    );     
+    wp_register_script(
+        'RPRHAGJsRun',
+        get_template_directory_uri().'/scripts/jsRun.js',
+        array('RPRHAGConfig')
+    );  
+        
+    wp_enqueue_script('dojo');
+    wp_enqueue_script('RPRHAGJsRun'); 
+}
+
+function LHM_stylesheet_loaderDojo(){
+    wp_register_style(
+        'claro',
+        '//ajax.googleapis.com/ajax/libs/dojo/1.9.1/dijit/themes/claro/claro.css',
+        array()
+    );
+    
+    wp_enqueue_style('claro');
+}
+
+function RPRHAG_stylesheet_loader(){
+    wp_register_style(
+        'RPRHAGstyle',
+        get_template_directory_uri().'/styles/styles.css',
+        array('claro')
+    );
+    
+    wp_enqueue_style('RPRHAGstyle');
+}
+
+add_action('wp_head', 'RPRHAG_stylesheet_loaderDojo', 1);
+add_action('wp_head', 'RPRHAG_stylesheet_loader', 5);  
+add_action('wp_head', 'RPRHAG_script_config', 2);
+add_action('wp_head', 'RPRHAG_script_loader', 5);  
+?>
---
+++ scripts/jsConfig.php
@@ -0,0 +1,31 @@
+<?php
+
+   //Let's set the header straight
+   header('Content-type: text/javascript');
+
+   //Get the WP-specifics, so that we can use constants and what not
+   $home_dir = preg_replace('^wp-content/themes/[a-zA-Z0-9\-/]+^', '', getcwd());
+   include($home_dir . 'wp-load.php');
+?>
+
+var themePath = "<?php echo get_template_directory_uri(); ?>";
+var pluginsPath = "<?php echo plugins_url(); ?>";
+var ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
+var customModulesPath = themePath + "/scripts";
+var dojoReleasePath = "/"
+
+var dojoConfig = {
+	"async": true,
+	"cacheBust": new Date(),
+	"parseOnLoad": false,
+    "baseUrl": customModulesPath+dojoReleasePath,
+    "dojoBlankHtmlUrl": "<?php echo get_template_directory_uri(); ?>/scripts/resources/blank.html",
+	"packages": [
+		{"name": "lib", "location": customModulesPath+dojoReleasePath+"lib"},
+		{"name": "simpo", "location": customModulesPath+dojoReleasePath+"simpo"},
+		{"name": "lh", "location": customModulesPath+dojoReleasePath+"rprhag"},
+        {"name": "dojo", "location": customModulesPath+dojoReleasePath+"dojo"},
+        {"name": "dijit", "location": customModulesPath+dojoReleasePath+"dijit"},
+        {"name": "dojox", "location": customModulesPath+dojoReleasePath+"dojox"}
+	]
+};
