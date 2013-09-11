733147510be1e9c2b31f05547cbd09bd3710cc3a
stephen <stephen@unallocated.barefruit.co.uk>
2013-09-11 19:45:25 +0100

    Added enqueuing scripts and dojo loader to the theme.

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/*  Theme functions
+ *  
+ *  @author Stephen Simpson <me@simpo.org>
+ *  @version v0.1
+ */
+add_theme_support('menus');
+
+require_once( get_template_directory() . '/includes/enqueue.php' );
+?>
---
+++ scripts/jsRun.js
@@ -0,0 +1,8 @@
+require([
+	"dojo/parser",
+	"dojo/ready"
+], function(parser,ready){
+	ready(function(){
+		parser.parse();
+	});
+});
