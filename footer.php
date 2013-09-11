2b83b9c81196d26f5f03daf05794ebd205482061
stephen <stephen@unallocated.barefruit.co.uk>
2013-09-11 19:11:56 +0100

    Basic stub files, no content.

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php
+/*  Theme main footer block
+ *  
+ *  @author Stephen Simpson <me@simpo.org>
+ *  @version v0.1
+ */
+?>
+<footer>
+    <?php wp_footer(); ?>
+</footer>
+</html>
---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+/*  Theme main header block
+ *  
+ *  @author Stephen Simpson <me@simpo.org>
+ *  @version v0.1
+ */
+?>
+<html>
+<head>
+    <title></title>
+    <?php wp_meta(); ?>
+    <?php wp_head(); ?>
+</head>
+<header>
+</header>
