dfc9776d0bb9139326af31d0beb5a6b104450ee6
stephen <stephen@unallocated.barefruit.co.uk>
2013-09-11 20:41:47 +0100

    Twaeked the enqueuing parameters.

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -19,7 +19,7 @@ function RPRHAG_script_loader(){
     wp_register_script(
         'RPRHAGJsRun',
         get_template_directory_uri().'/scripts/jsRun.js',
-        array('RPRHAGConfig')
+        array('RPRHAGConfig','dojo')
     );  
         
     wp_enqueue_script('dojo');
@@ -30,7 +30,8 @@ function LHM_stylesheet_loaderDojo(){
     wp_register_style(
         'claro',
         '//ajax.googleapis.com/ajax/libs/dojo/1.9.1/dijit/themes/claro/claro.css',
-        array()
+        array(),
+        '1.9.1'
     );
     
     wp_enqueue_style('claro');
@@ -39,8 +40,7 @@ function LHM_stylesheet_loaderDojo(){
 function RPRHAG_stylesheet_loader(){
     wp_register_style(
         'RPRHAGstyle',
-        get_template_directory_uri().'/styles/styles.css',
-        array('claro')
+        get_template_directory_uri().'/styles/styles.css'
     );
     
     wp_enqueue_style('RPRHAGstyle');
