56e286fb421c28bd7a4af0df034da79d360ab01d
Stephen Simpson <me@simpo.org>
2013-09-12 18:52:40 +0100

    Fixed the dojo stylesheet not loading due to copy'n'paste error.

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -26,7 +26,7 @@ function RPRHAG_script_loader(){
     wp_enqueue_script('RPRHAGJsRun'); 
 }
 
-function LHM_stylesheet_loaderDojo(){
+function RPRHAG_stylesheet_loaderDojo(){
     wp_register_style(
         'claro',
         '//ajax.googleapis.com/ajax/libs/dojo/1.9.1/dijit/themes/claro/claro.css',
@@ -40,7 +40,9 @@ function LHM_stylesheet_loaderDojo(){
 function RPRHAG_stylesheet_loader(){
     wp_register_style(
         'RPRHAGstyle',
-        get_template_directory_uri().'/styles/styles.css'
+        get_template_directory_uri().'/styles/styles.css',
+        array(),
+        '1.1'
     );
     
     wp_enqueue_style('RPRHAGstyle');
