78c24c4e39e56a4e69ed6d3e3b71cab57127a144
stephen <stephen@unallocated.barefruit.co.uk>
2013-09-12 18:51:13 +0100

    Changed the slideshow script that is being used to a better one (taken from LukeHowellsMagic.co.uk).

--- content.php
+++ content.php
@@ -6,7 +6,24 @@
  */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <img class="left" src="/wp-content/themes/RPRHAG/media/images/slideshow/3.jpg" height="350" width="350" style="margin-right: 20px" />
+    <div
+        class="dojoLhSlideshow"
+style="float:left;margin-right:20px;background-image: url('<?php echo get_template_directory_uri(); ?>/media/images/slideshow/KayleighAndJosephine.jpg');"
+	data-dojo-type="rprhag/slideshow2"
+	data-dojo-props="src:[
+            themePath+'/media/images/slideshow/KayleighAndJosephine.jpg',
+            themePath+'/media/images/slideshow/LukeGreen.jpg',
+            themePath+'/media/images/slideshow/Banners.jpg',
+            themePath+'/media/images/slideshow/ChrisAndEloise.jpg',
+            themePath+'/media/images/slideshow/Geoff.jpg',
+            themePath+'/media/images/slideshow/Coffee.jpg',
+            themePath+'/media/images/slideshow/Luke.jpg',
+            themePath+'/media/images/slideshow/Stephen.jpg',
+            themePath+'/media/images/slideshow/Mal.jpg',
+            themePath+'/media/images/slideshow/Reception.jpg',
+            themePath+'/media/images/slideshow/Sarah.jpg'
+	],slices:25,width:350,height:350"
+	></div>
     <header>
         <h1><?php the_title(); ?></h1>
     </header>
