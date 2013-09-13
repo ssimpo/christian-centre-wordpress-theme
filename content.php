0df80a96a300674b4b4819989dc848be5bdc41e6
stephen <stephen@unallocated.barefruit.co.uk>
2013-09-13 21:39:50 +0100

    Replaced the slideshow with new one from the simpo repository.

--- content.php
+++ content.php
@@ -6,23 +6,25 @@
  */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <div
-        class="dojoLhSlideshow"
-style="float:left;margin-right:20px;background-image: url('<?php echo get_template_directory_uri(); ?>/media/images/slideshow/KayleighAndJosephine.jpg');"
-	data-dojo-type="rprhag/slideshow2"
-	data-dojo-props="src:[
-            themePath+'/media/images/slideshow/KayleighAndJosephine.jpg',
-            themePath+'/media/images/slideshow/LukeGreen.jpg',
-            themePath+'/media/images/slideshow/Banners.jpg',
-            themePath+'/media/images/slideshow/ChrisAndEloise.jpg',
-            themePath+'/media/images/slideshow/Geoff.jpg',
-            themePath+'/media/images/slideshow/Coffee.jpg',
-            themePath+'/media/images/slideshow/Luke.jpg',
-            themePath+'/media/images/slideshow/Stephen.jpg',
-            themePath+'/media/images/slideshow/Mal.jpg',
-            themePath+'/media/images/slideshow/Reception.jpg',
-            themePath+'/media/images/slideshow/Sarah.jpg'
-	],slices:25,width:350,height:350"
+        <div
+            data-dojo-type="simpo/widget/slideshow"
+            data-dojo-props="src:[
+                    themePath+'/media/images/slideshow/KayleighAndJosephine.jpg',
+                    themePath+'/media/images/slideshow/LukeGreen.jpg',
+                    themePath+'/media/images/slideshow/Banners.jpg',
+                    themePath+'/media/images/slideshow/ChrisAndEloise.jpg',
+                    themePath+'/media/images/slideshow/Geoff.jpg',
+                    themePath+'/media/images/slideshow/Coffee.jpg',
+                    themePath+'/media/images/slideshow/Luke.jpg',
+                    themePath+'/media/images/slideshow/Stephen.jpg',
+                    themePath+'/media/images/slideshow/Mal.jpg',
+                    themePath+'/media/images/slideshow/Reception.jpg',
+                    themePath+'/media/images/slideshow/Sarah.jpg'
+                ],
+                type:'squares',
+                squaresSize:36,
+                interval:2500
+            "
 	></div>
     <header>
         <h1><?php the_title(); ?></h1>
