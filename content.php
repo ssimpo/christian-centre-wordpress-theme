dc0f3df38808ffc9ae4705205fd4cd264097c7d7
stephen <stephen@unallocated.barefruit.co.uk>
2013-09-14 07:50:44 +0100

    Refactored to move the homepage slideshow into its own file.

--- content.php
+++ content.php
@@ -6,26 +6,9 @@
  */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <div
-            data-dojo-type="simpo/widget/slideshow"
-            data-dojo-props="src:[
-                    themePath+'/media/images/slideshow/KayleighAndJosephine.jpg',
-                    themePath+'/media/images/slideshow/LukeGreen.jpg',
-                    themePath+'/media/images/slideshow/Banners.jpg',
-                    themePath+'/media/images/slideshow/ChrisAndEloise.jpg',
-                    themePath+'/media/images/slideshow/Geoff.jpg',
-                    themePath+'/media/images/slideshow/Coffee.jpg',
-                    themePath+'/media/images/slideshow/Luke.jpg',
-                    themePath+'/media/images/slideshow/Stephen.jpg',
-                    themePath+'/media/images/slideshow/Mal.jpg',
-                    themePath+'/media/images/slideshow/Reception.jpg',
-                    themePath+'/media/images/slideshow/Sarah.jpg'
-                ],
-                type:'squares',
-                squaresSize:36,
-                interval:2500
-            "
-	></div>
+<?php if(is_home() || is_front_page()){
+    get_template_part( 'content', 'homepage' );
+}?>
     <header>
         <h1><?php the_title(); ?></h1>
     </header>
--- index.php
+++ index.php
@@ -7,13 +7,7 @@
 ?>
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php
-	if(is_home() || is_front_page()){
-		get_template_part( 'content', 'homepage' );
-	}else{
-		get_template_part( 'content', get_post_format() );
-	}
-?>
+<?php get_template_part( 'content', get_post_format() );?>
 <?php endwhile; else: ?>
 <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 <?php endif; ?>
