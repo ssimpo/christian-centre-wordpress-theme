eb21899419b1bc928d47cf996b548dbab9f1d613
stephen <stephen@unallocated.barefruit.co.uk>
2013-09-14 15:53:50 +0100

    Changed the metro layout to make the icon as the background and the link text to the <div> element.  Deleted a load of unused icons from the theme and moved the used ones to a subdirectory of styles andout of the media folders. Created a new LessCSS file for the metro icon backgrounds.

--- header.php
+++ header.php
@@ -17,22 +17,22 @@
 <header>
 	<nav class="metromenu" data-dojo-type="simpo/widget/expandingDiv">
 		<a href="<?php echo home_url();?>" class="logo"><img src="<?php echo get_template_directory_uri(); ?>/media/images/logos/tcc-black.png" width="255" height="117" class="logo" alt="The Christian Centre Logo (Green version)" /></a>
-		<a href="/contactus" class="contact green metro"><div>&nbsp;</div>Contact Us</a>
-		<a href="/events" class="events blue metro"><div>&nbsp;</div>Events</a>
-		<a href="/children" class="children orange metro"><div>&nbsp;</div>Children</a>
-		<a href="/students" class="students green metro"><div>&nbsp;</div>Students</a>
-		<a href="/podcast" class="podcast orange metro"><div>&nbsp;</div>Podcast</a>
-		<a href="/prayer" class="prayer orange metro"><div>&nbsp;</div>Prayer</a>
-		<a href="/the_team" class="team blue metro"><div>&nbsp;</div>The Team</a>
-		<a href="/vision" class="vision green metro"><div>&nbsp;</div>Vision</a>
-		<a href="/statement_of_faith" class="statement orange metro"><div>&nbsp;</div>Statement of Faith</a>
-		<a href="/history" class="history blue metro"><div>&nbsp;</div>History</a>
-		<a href="/students" class="students green metro"><div>&nbsp;</div>Students</a>
-		<a href="/podcast" class="podcast orange metro"><div>&nbsp;</div>Podcast</a>
-		<a href="/prayer" class="prayer orange metro"><div>&nbsp;</div>Prayer</a>
-		<a href="/the_team" class="team blue metro"><div>&nbsp;</div>The Team</a>
-		<a href="/vision" class="vision green metro"><div>&nbsp;</div>Vision</a>
-		<a href="/statement_of_faith" class="statement orange metro"><div>&nbsp;</div>Statement of Faith</a>
-		<a href="/history" class="history blue metro"><div>&nbsp;</div>History</a>
+		<a href="/contactus" class="contact green metro"><div>Contact Us</div></a>
+		<a href="/events" class="events blue metro"><div>Events</div></a>
+		<a href="/children" class="children orange metro"><div>Children</div></a>
+		<a href="/students" class="students green metro"><div>Students</div></a>
+		<a href="/podcast" class="podcast orange metro"><div>Podcast</div></a>
+		<a href="/prayer" class="prayer orange metro"><div>Prayer</div></a>
+		<a href="/the_team" class="team blue metro"><div>The Team</div></a>
+		<a href="/vision" class="vision green metro"><div>Vision</div></a>
+		<a href="/statement_of_faith" class="statement orange metro"><div>Statement of Faith</div></a>
+		<a href="/history" class="history blue metro"><div>History</div></a>
+		<a href="/students" class="students green metro"><div>Students</div></a>
+		<a href="/podcast" class="podcast orange metro"><div>Podcast</div></a>
+		<a href="/prayer" class="prayer orange metro"><div>Prayer</div></a>
+		<a href="/the_team" class="team blue metro"><div>The Team</div></a>
+		<a href="/vision" class="vision green metro"><div>Vision</div></a>
+		<a href="/statement_of_faith" class="statement orange metro"><div>Statement of Faith</div></a>
+		<a href="/history" class="history blue metro"><div>History</a>
 	</nav>
 </header>
