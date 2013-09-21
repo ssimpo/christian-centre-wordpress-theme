8f8c8409f362d7f7269f979d1eada4bc880f8a04
stephen <stephen@unallocated.barefruit.co.uk>
2013-09-21 12:38:29 +0100

    Added some of the new slideshow images to the slideshow.

--- content-homepage.php
+++ content-homepage.php
@@ -12,14 +12,18 @@
 	data-dojo-props="src:[
 		themePath+'/media/images/slideshow/KayleighAndJosephine.jpg',
 	    themePath+'/media/images/slideshow/LukeGreen.jpg',
+		themePath+'/media/images/slideshow/MiddlesbroughCollege.jpg',
 	    themePath+'/media/images/slideshow/Banners.jpg',
+		themePath+'/media/images/slideshow/Luke.jpg',
+		themePath+'/media/images/slideshow/BottleOfNotes.jpg',
 	    themePath+'/media/images/slideshow/ChrisAndEloise.jpg',
+		themePath+'/media/images/slideshow/Kids.jpg',
 	    themePath+'/media/images/slideshow/Geoff.jpg',
 	    themePath+'/media/images/slideshow/Coffee.jpg',
-	    themePath+'/media/images/slideshow/Luke.jpg',
+		themePath+'/media/images/slideshow/GeoffTea.jpg',
 	    themePath+'/media/images/slideshow/Stephen.jpg',
-	    themePath+'/media/images/slideshow/Mal.jpg',
-	    themePath+'/media/images/slideshow/Reception.jpg',
+		themePath+'/media/images/slideshow/Mal.jpg',
+		themePath+'/media/images/slideshow/MiddlesbroughCollegeSign.jpg',
 	    themePath+'/media/images/slideshow/Sarah.jpg'
 	], type:'squares', squaresSize:36, interval:2500"
 	class="simpoSlideshow homepageSlideshow"
