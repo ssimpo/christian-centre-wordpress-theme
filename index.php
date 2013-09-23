a81982389e1aead501bd9a40e75c6130864b1f07
stephen <stephen@unallocated.barefruit.co.uk>
2013-09-23 11:50:13 +0100

    Added post ordering to the homepage.

--- index.php
+++ index.php
@@ -9,6 +9,14 @@
     get_header();
     $postSeq = 0;
     ?><div class="articles"><?php
+    if(is_home() || is_front_page() || in_category('Homepage')){
+        query_posts(array(
+            'orderby' => 'meta_value_num',
+            'meta_key' => 'RPRHAG_post_order',
+            'order' => 'ASC',
+            'posts_per_page' => 8
+        ));
+    }
     if ( have_posts() ) :
         while ( have_posts() ) : the_post();
             if(is_home() || is_front_page()){
