60e35bc7220d4c0011a2beb1f1452a29a5f258a8
Stephen Simpson <me@simpo.org>
2013-09-27 13:23:12 +0100

    Added ability to turn-off <article> and/or <h1> on pages.  This is usefu if a shortcde embeds a series or articles.  Also added some styling for sermons.

--- content.php
+++ content.php
@@ -15,12 +15,23 @@ if(is_home() || is_front_page()){
         get_template_part( 'content', 'homepage' );
     }
 }
-?>
+
+$showArticle = get_post_meta(get_the_ID(), 'RPRHAG_post_show_article', true);
+$showArticle = ((!$showArticle)?"Yes":$showArticle);
+$showHeading = get_post_meta(get_the_ID(), 'RPRHAG_post_show_heading', true);
+$showHeading = ((!$showHeading)?"Yes":$showHeading);
+
+if ($showArticle === "Yes") { ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php } ?>
     <header>
+        <?php if ($showHeading === "Yes") { ?>
         <h1><?php the_title(); ?></h1>
+        <?php } ?>
     </header>
     <?php the_content(); ?>
     <footer>
     </footer>
+<?php if ($showArticle === "Yes") { ?>
 </article>
+<?php } ?>
