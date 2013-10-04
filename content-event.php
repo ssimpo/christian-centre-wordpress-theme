3824c2626d6540259ba8136983b8d750ece61556
stephen <stephen@unallocated.barefruit.co.uk>
2013-10-04 18:28:35 +0100

    Added microformat data and <time> tags.

--- content-event.php
+++ content-event.php
@@ -11,25 +11,28 @@
 <?php if(is_home() || is_front_page() || ($RPRHAG_category[0]->cat_ID === get_cat_ID('Homepage'))){
     get_template_part( 'content', 'homepage' );
 }?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('hentry'); ?>>
     <header>
-        <h1 class="event-date-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <h1 class="event-date-title entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
         <p class="event-date-subtitle"><?php
             $current_occurance = eo_get_next_occurrence_of(get_the_ID());
             $start = $current_occurance['start']->format('jS M Y');
             $end = $current_occurance['end']->format('jS M Y');
+            $htmlStartTime = $current_occurance['start']->format('Y-m-d G:i:s');
+            $htmlEndTime = $current_occurance['end']->format('Y-m-d G:i:s');
             
             if ($start !== $end) {
-                echo $start . ' - ' . $end;
+                echo '<time datetime="'.$htmlStartTime.'">' . $start . '</time> - <time datetime="'.$htmlEndTime.'">' . $end .'</time>';
             } else {
                 $startTime = $current_occurance['start']->format('g:ia');
                 $endTime = $current_occurance['end']->format('g:ia');
                 
-                echo $start . ' ' . $startTime . '-' . $endTime;
+                echo '<time datetime="'.$htmlStartTime.'">' . $start . ' ' . $startTime . '</time>-<time datetime="'.$htmlEndTime.'">' . $endTime . '</time>';
             }
            
         ?></p>
     </header>
+    <!--<?php the_content(); ?>-->
     <?php the_excerpt(); ?>
     <p><a href="<?php the_permalink(); ?>">[More details ...]</a></p>
     
